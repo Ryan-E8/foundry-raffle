05916f9f2f93acd88de2bd0247e5066d4facee84
Ryan <ryanelliott.re1@gmail.com>
2023-07-22 10:32:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Proveably Random Raffle Contract
+
+## About
+
+This code is to create a proveably random smart contract lottery.
+
+## What do we want it to do?
+
+1. Users can enter by paying for a ticket.
+   1. The ticket fees are going to go to the winner during the draw
+2. After X period of time, the lottery will automatically draw a winner
+   1. This will be done programatically
+3. Using Chainlink VRF & Chainlink Automation
+   1. Chainlink VRF -> Randomness
+   2. Chainlink Automation -> Time based trigger
+
+
+## Tests!
+
+1. Deploy scripts
+2. Tests
+   1. Work on a local chain
+   2. Forked Testnet
+   3. Forked Mainnet
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
+
+contract HelperConfig is Script {
+    // 1. Deploy mocks when we are on a local anvil chain
+    // 2. Keep track of contract addresses across different chains
+
+    NetworkConfig public activeNetworkConfig;
+    uint256 public constant DEFAULT_ANVIL_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+        address link;
+        uint256 deployerKey;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+                subscriptionId: 3772, // Update this with our SubId
+                callbackGasLimit: 50000, // 500,000 gas
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                deployerKey: vm.envUint("PRIVATE_KEY")
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        uint96 baseFee = 0.25 ether; // 0.25 LINK
+        uint96 gasPriceLink = 1e9; // 1 gwei LINK
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinator = new VRFCoordinatorV2Mock(
+            baseFee,
+            gasPriceLink
+        );
+        LinkToken link = new LinkToken();
+        vm.stopBroadcast();
+
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: address(vrfCoordinator),
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // Does not matter so we can keep it like this
+                subscriptionId: 0, // Our script will have this
+                callbackGasLimit: 50000,
+                link: address(link), // 500,000 gas
+                deployerKey: DEFAULT_ANVIL_KEY
+            });
+    }
+}
